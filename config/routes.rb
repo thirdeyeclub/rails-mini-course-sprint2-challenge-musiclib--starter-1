c56db430aa1e6ca9c0e7f5c29e89b482c0529c56
Indie Blount <wwwaaavvveeerrr@gmail.com>
2019-08-23 12:29:37 -0400

    build routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,14 @@ Rails.application.routes.draw do
         resources :albums, only: [:index]
       end
       resources :songs, only: [:index, :show]
-      resources :users, only: [:index, :show, :create]
+      resources :users, only: [:index, :show, :create] do
+        post 'playlists', to: 'playlists#create'
+        get 'playlists', to: 'playlists#index'
+      end
+      resources :playlists, only: [:index, :show, :create] do
+        post 'songs', to: 'songs#create'
+        get 'songs', to: 'songs#index'
+      end
     end
   end
 end
